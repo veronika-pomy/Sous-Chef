a0fbc81ec1983539ab715cd769ea606a0c174f43
Variegatedhuman <116999437+Variegatedhuman@users.noreply.github.com>
2023-01-08 17:48:10 -0600

    initial commit to new-feature branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-const searcheBtn = document.querySelector(".searchBtn")
+const searchBtn = document.querySelector(".searchBtn")
 const searchEl = document.querySelector(".starter")
 const searchedItem = document.querySelector(".searchThis")
 const mainContentEl = document.querySelector(".mainContent")
 const videoDisplayEl = document.querySelector(".videoDisplay")
 const nutritionDisplayEl = document.querySelector(".nutritionDisplay")
 
-searcheBtn.addEventListener("click", searchRecipe)
+searchBtn.addEventListener("click", searchRecipe)
 
 function searchRecipe() {
     if (searchedItem) {
